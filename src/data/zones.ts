57d66556e49c5708a6ef6a45545cfc509b6aaff5
robertarnorsson <rha006@gmail.com>
2024-11-25 23:48:03 +0100

    Added zones data. Swamps filled out

---
+++ src/data/zones.ts
@@ -0,0 +1,364 @@
+export const zones = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      id: "01",
+      properties: { name: "Swamps" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [26555, 9624],
+            [25597, 9717],
+            [24918, 10047],
+            [24308, 10009],
+            [23868, 9562],
+            [23704, 9206],
+            [23206, 8685],
+            [23286, 8295],
+            [23575, 8044],
+            [23924, 7885],
+            [24825, 7267],
+            [25142, 7236],
+            [25538, 7273],
+            [25908, 7238],
+            [26246, 7071],
+            [26588, 6972],
+            [26717, 7088],
+            [26582, 7611],
+            [26714, 8084],
+            [26669, 8345],
+            [26562, 8618],
+            [26501, 8955],
+            [26452, 9308],
+            [26555, 9624],
+          ]
+        ],
+      },
+    },
+    {
+      type: "Feature",
+      id: "02",
+      properties:{ name: "Backwater" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "03",
+      properties:{ name: "Sircaa" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "04",
+      properties:{ name: "Wild Island" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "05",
+      properties:{ name: "Cordon" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "06",
+      properties:{ name: "Lesser Zone" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "07",
+      properties:{ name: "Burnt Forest" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "08",
+      properties:{ name: "Duga" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "09",
+      properties:{ name: "Malachite" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "10",
+      properties:{ name: "Chemical Plant" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "11",
+      properties:{ name: "Garbage" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "12",
+      properties:{ name: "Cement Factory" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "13",
+      properties:{ name: "Cooling Towers" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "14",
+      properties:{ name: "Rostok" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "15",
+      properties:{ name: "Yantar" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "16",
+      properties:{ name: "Red Forest" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "17",
+      properties:{ name: "Yaniv" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "18",
+      properties:{ name: "Jupiter" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "19",
+      properties:{ name: "Generators" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      id: "20",
+      properties:{ name: "Prypiat" },
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [0,0],
+            [1,0],
+            [0,1],
+            [1,1],
+            [0,0]
+          ]
+        ]
+      }
+    },
+  ],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from 'hono'
+import { zones } from './data/zones';
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Stalker 2 API')
-})
+});
+
+app.use('*', (c, next) => {
+  c.header('Access-Control-Allow-Origin', '*');
+  c.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  c.header('Access-Control-Allow-Headers', 'Content-Type');
+  return next();
+});
+
+app.get('/zones', (c) => {
+  return c.json(zones);
+});
 
 export default app
